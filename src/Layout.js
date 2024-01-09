b80b0b7b7cd391c5032b98d683a70c463bb65a3d
Saurabh Shrikhande <saurabhshrikhande@200gmail.com>
2024-01-10 02:21:37 +0530

    half

---
+++ src/Layout.js
@@ -0,0 +1,20 @@
+import Header from "./Header";
+// import Footer from './Footer';
+
+export default function Layout(){
+    return ( <div>
+
+<Header
+    logo = "https://flowbite.com/docs/images/logo.svg"
+    title = "GeekFoods"
+     home = "Home"
+     quote = "Quote" 
+     resturants = "Resturants"
+     foods = "Foods"
+     contact = "Contact"
+    />
+
+       {/* <Footer /> */}
+
+        </div> );
+}
