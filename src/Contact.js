d7422ece817ffce808d8b03233e86736928d549b
Saurabh Shrikhande <saurabhshrikhande200@gmail.com>
2024-01-10 18:25:22 +0530

    contact page html css

--- src/Contact.js
+++ src/Contact.js
@@ -1,9 +1,44 @@
 export default function Contact(){
-    return (<div style={{minHeight:"100vh"}}>
+    return (<div style={{minHeight:"100vh" , padding:"20px 20vw", display:"flex", gap:"10vw"}}>
      
-     <h1>Mo No</h1>
-     <h1>8888237924</h1>
-     <br/>
+
+     <div>
+     <h1>Contact Us</h1>
+     <h1>GET IN TOUCH WITH US</h1>
      
+    <br/>
+
+    <p style={{width:"25vw"}}>orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eius tempor incididunt ut labore et dolore magna aliqua. Ut enim adiqua minim veniam quis nostrud exercitation ullamco</p>
+    
+    <h1>Our Location</h1>
+    <p>99 S.t Jomblo Park Pekanbaru 28292. <br/> Indonesia</p>
+    
+    <h1>Phone Number</h1>
+    <p>(+62)81 414 257 9980</p>
+
+
+    <h1>Email Address</h1>
+    <p>info@yourdomain.com</p>
+    </div>
+
+    <div style={{fontSize:"25px", marginTop:"10vh"}}>
+        <form style={{backgroundColor:"#e6e6e6", padding:"5vh 5vw", borderRadius:"20px"}}>
+            <label style={{ marginRight:"25px"}}>Name</label>
+            <input style={{ height:"25px", width:"200px"}} type="text" />
+            <div style={{margin:"25px"}}></div>
+        
+            <label  style={{marginRight:"25px"} }>Email</label>
+            <input style={{ height:"25px", width:"200px"}} type="text" />
+            <div style={{margin:"25px"}}></div>
+  
+            <label  style={{marginRight:"25px"}}>Your phone</label>
+            <input style={{ height:"25px", width:"200px"}} type="text" />
+            <div style={{margin:"25px"}}></div>
+
+            <label  style={{marginRight:"25px"}}>Your Massage</label>
+            <textarea ></textarea>
+        </form>
+    </div>
+
     </div>)
 }
